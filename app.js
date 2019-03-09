fdded5acb7fab79cfd760b0f0ac04ceca06e8593
edjunma <junwenma@buffalo.edu>
2019-03-08 21:20:54 -0500

    Finished build-v1

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // Listen for submit
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating...");
-
   // UI Vars
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
@@ -19,7 +26,7 @@ function calculateResults(e) {
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
-  // Compute monthly payments
+  // Compute monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal * x * calculatedInterest) / (x - 1);
 
@@ -27,15 +34,25 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show results
+    document.getElementById("results").style.display = "block";
+
+    // Hide loader
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers");
   }
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Hide loader
+  document.getElementById("loading").style.display = "none";
+
   // Create a div
   const errorDiv = document.createElement("div");
 
@@ -56,7 +73,7 @@ function showError(error) {
   setTimeout(clearError, 3000);
 }
 
-// Clear Error
+// Clear error
 function clearError() {
   document.querySelector(".alert").remove();
 }
